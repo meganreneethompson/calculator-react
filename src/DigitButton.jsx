f60e26f56f3a461e30cc34d592833187ac3d1f0a
Meg Thompson <128401836+meganreneethompson@users.noreply.github.com>
2024-03-14 12:54:15 -0400

    operation buttons

--- src/DigitButton.jsx
+++ src/DigitButton.jsx
@@ -1,8 +1,12 @@
 import { ACTIONS } from "./App"
 
 export default function DigitButton({ dispatch, digit }) {
-   <button onClick={() => dispatch({ type ACTIONS.ADD_DIGIT, payload: {digit} })}> {digit}</button>
-   
-    return
-}
+    return (
+      <button
+        onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+      >
+        {digit}
+      </button>
+    )
+  }
 
